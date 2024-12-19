c76066570cc706b4f8902e2e16a404872243f318
Yunus Hanifi <oyunushanifi@gmail.com>
2024-12-19 13:26:07 +0300

    feat: add DELETE and PUT routes for product management with error handling

--- src/app/api/products/[id]/route.js
+++ src/app/api/products/[id]/route.js
@@ -22,3 +22,44 @@ export async function GET(request, { params }) {
     }
 
 }
+
+export async function DELETE(request, { params }) {
+    const { id } = await params;
+    try {
+        const client = await clientPromise;
+        const db = client.db('yazlab1-3');
+
+        const product = await db.collection('products').deleteOne({ _id: new ObjectId(id) });
+
+        if (product.deletedCount === 0) {
+            return new Response(JSON.stringify({ error: 'Product not found' }), { status: 404 });
+        }
+
+        return new Response(JSON.stringify({ message: 'Product deleted' }), { status: 200 });
+    }
+    catch (error) {
+        console.error("Error during delete:", error);
+        return new Response(JSON.stringify({ error: 'Failed to delete product' }), { status: 500 });
+    }
+}
+
+export async function PUT(request, { params }) {
+    const { id } = await params;
+    const { name, price } = await request.json();
+    try {
+        const client = await clientPromise;
+        const db = client.db('yazlab1-3');
+
+        const product = await db.collection('products').updateOne({ _id: new ObjectId(id) }, { $set: { name, price } });
+
+        if (product.matchedCount === 0) {
+            return new Response(JSON.stringify({ error: 'Product not found' }), { status: 404 });
+        }
+
+        return new Response(JSON.stringify({ message: 'Product updated' }), { status: 200 });
+    }
+    catch (error) {
+        console.error("Error during update:", error);
+        return new Response(JSON.stringify({ error: 'Failed to update product' }), { status: 500 });
+    }
+}
